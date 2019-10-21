bc73bc8975ae117b26b4ad0c7818118667d1b956
Markus Malz <markus.malz@it2media.de>
2019-10-21 15:22:44 +0200

    Convert to library

--- Bash/docker_stop.sh
+++ Bash/docker_stop.sh
@@ -1,7 +1,19 @@
 #!/usr/bin/env bash
+#
+# Stops a docker container by entrypoint name and port
 
-for container_id in $(docker container ls -a --no-trunc | awk 'FNR == 1 {next}{print $1}')
-do
+#######################################
+# Stops a docker container by entrypoint name and port
+# Arguments:
+#   $1: The docker containers entrypoint process name
+#   $2: The published port to search for
+# Returns:
+#   None
+#######################################
+docker_stop() {
+  for container_id in $(docker container ls -a --no-trunc | awk 'FNR == 1 {next}{print $1}')
+  do
     docker_inspect_json=$(docker inspect "$n")
     echo "$1 $2 $container_id $docker_inspect_json"
-done
+  done
+}
