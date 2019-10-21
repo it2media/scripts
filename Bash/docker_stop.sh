38f8a403ee2ab078aa86a7d4129f132723fef0ad
Markus Malz <markus.malz@it2media.de>
2019-10-21 16:15:36 +0200

    Test passing the params and jq

--- Bash/docker_stop.sh
+++ Bash/docker_stop.sh
@@ -2,6 +2,12 @@
 #
 # Stops a docker container by entrypoint name and port
 
+# prüfen ob die Installation von jq dokumentiert ist, oder per bootsteapping ins ci.yml einbauen
+docker_stop_running() {
+  current_name=$(echo "$4" | jq -r '.[0] | .Name')
+  echo "$1 $2 $3 $current_name"
+}
+
 #######################################
 # Stops a docker container by entrypoint name and port
 # Arguments:
@@ -14,6 +20,6 @@ docker_stop() {
   for container_id in $(docker container ls -a --no-trunc | awk 'FNR == 1 {next}{print $1}')
   do
     docker_inspect_json=$(docker inspect "$container_id")
-    echo "$1 $2 $container_id $docker_inspect_json"
+    docker_stop_running "$1 $2 $container_id $docker_inspect_json"
   done
 }
