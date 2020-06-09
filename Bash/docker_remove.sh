8e087d7e40940a2fae46a63dadd133d4dbe1a410
Markus Malz <markus.malz@it2media.de>
2020-06-09 08:16:39 +0200

    Define variables local inside functions

--- Bash/docker_remove.sh
+++ Bash/docker_remove.sh
@@ -11,10 +11,10 @@
 #   Exit code 0 on success, 1 on failure
 #######################################
 docker_is_running() {
-  service=$(echo "$2" | jq -r '.[0] | .Config.Labels["com.docker.compose.service"]')
+  local service=$(echo "$2" | jq -r '.[0] | .Config.Labels["com.docker.compose.service"]')
   if [[ "$service" == "$1" ]]; then
-      has_state_running=$(echo "$2" | jq -r '.[0] | .State.Running')
-      status=$(echo "$2" | jq -r '.[0] | .State.Status')
+      local has_state_running=$(echo "$2" | jq -r '.[0] | .State.Running')
+      local status=$(echo "$2" | jq -r '.[0] | .State.Status')
       if [[ "$has_state_running" == true && "$status" == "running" ]]; then
         return 0 # 0 is success exit code so equivalent to true
       fi
@@ -32,7 +32,7 @@ docker_is_running() {
 docker_exists_running() {
   for container_id in $(docker container ls -a --no-trunc | awk 'FNR == 1 {next}{print $1}')
   do
-    docker_inspect_json=$(docker inspect "$container_id")
+    local docker_inspect_json=$(docker inspect "$container_id")
     docker_is_running "$1" "$docker_inspect_json" && return 0 # if we found any running container for this service, we return true
   done
   return 1 # if no running container was found for this service name we return false
@@ -46,7 +46,14 @@ docker_exists_running() {
 #   None
 #######################################
 docker_remove_stopped() {
-  echo "Not implemented yet"
+  local service=$(echo "$1" | jq -r '.[0] | .Config.Labels["com.docker.compose.service"]')
+  echo "$service"
+  docker_exists_running "$service"
+  if [[ $? -eq 0 ]]; then
+    echo "There is a running service with the same service name"
+  else
+    echo "No running service found with service name: $1"
+  fi
 }
 
 #######################################
@@ -59,7 +66,7 @@ docker_remove_stopped() {
 docker_remove() {
   for container_id in $(docker container ls -a --no-trunc | awk 'FNR == 1 {next}{print $1}')
   do
-    docker_inspect_json=$(docker inspect "$container_id")
+    local docker_inspect_json=$(docker inspect "$container_id")
     docker_remove_stopped "$docker_inspect_json"
   done
 }
