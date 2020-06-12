e71f32090e1aef02c6bffa7a8db036d49eb5c92b
Markus Malz <markus.malz@it2media.de>
2020-06-12 13:30:41 +0200

    Script from .gitlab-ci.yml as reusable method

--- Bash/docker_stop.sh
+++ Bash/docker_stop.sh
@@ -54,3 +54,31 @@ docker_stop() {
     docker_stop_running "$1" "$2" "$container_id" "$docker_inspect_json"
   done
 }
+
+#######################################
+# Pulls a docker image, stops any running container of this type and composes a new one based on the newer image
+# Arguments:
+#   $1: The docker registry URL without leading https://, e.g. registry.it2media.de
+#   $2: Username for docker registry, e.g. it2media
+#   $3: Path to password file for container registry e.g. /mnt/salesdatacore/ContainerRegistry/login/it2media
+#   $4: Imagename (with optional tag) to pull: e.g. salesdatacore:latest
+#   $5: Docker compose service name: e.g. salesdatacore-dev
+#   $6: Relativ path to docker-compose file in git repository: e.g. docker-compose.dev.yml
+#   $7: The docker containers entrypoint process name of the to stop container, e.g. IT2media.SalesDataCore.dll
+#   $8: The published port of the to stop container, e.g. 24143
+# Returns:
+#   None
+#######################################
+docker_pull_stop_and_run() {
+  echo "Pull environment image and copy latest compose file for this environment"
+  cat "$3" | docker login --username "$2" --password-stdin "$1"
+  docker pull "$4"
+  mkdir "$5"
+  mv "$6" "$5/docker-compose.yml"
+  echo "Load latest stop script and stop container with specific entrypoint and port and start a new one with the docker-compose file for this environment"
+  cd "$5"
+  curl -o docker_stop.sh https://raw.githubusercontent.com/it2media/scripts/master/Bash/docker_stop.sh
+  . docker_stop.sh
+  docker_stop "$7" "$8"
+  docker-compose --project-name "$(basename $PWD)-$(date +%Y%m%d%H%M%S)" up -d
+}
